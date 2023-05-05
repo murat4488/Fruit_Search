a4624d640f55205af91c7c4e681ca5184c36b5cb
murat4488 <muratguney@msn.com>
2023-05-05 10:39:37 -0400

    Added Fruit Search Exercise

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	// TODO
+	results = [...fruit.filter( val => val.toLowerCase().includes(str.toLowerCase()))];
+	return results;
+}
+
+
+function searchHandler(e) {
+	const suggestedItems = search(e.target.value);
+	showSuggestions(suggestedItems);
+}
+
+function showSuggestions(results) {
+	suggestions.innerHTML = '';
+	results.map(i => {
+		const li = document.createElement('li');
+		li.innerText = i;
+		li.addEventListener('click', useSuggestion);
+		suggestions.append(li);
+	})
+}
+
+function useSuggestion(e) {
+	suggestions.innerHTML = '';
+	input.value = e.target.innerText;
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);

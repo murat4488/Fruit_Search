6bb25e1eb5743a83cec3c143c2ea1071fe162b48
murat4488 <muratguney@msn.com>
2023-05-26 14:19:53 -0400

    update codes

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
+	
 	// TODO
-	results = [...fruit.filter( val => val.toLowerCase().includes(str.toLowerCase()))];
-	return results;
+	return fruit.filter( val => val.toLowerCase().includes(str.toLowerCase()));
+	
 }
 
 
@@ -18,7 +18,7 @@ function searchHandler(e) {
 
 function showSuggestions(results) {
 	suggestions.innerHTML = '';
-	results.map(i => {
+	results.forEach(i => {
 		const li = document.createElement('li');
 		li.innerText = i;
 		//li.addEventListener('click', useSuggestion);
